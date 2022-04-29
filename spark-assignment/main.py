3bf4ac0c67d604e125779af972fb34d5c7692600
Varun Kumar Kumar <x030388@APLKLBT375291.local>
2022-04-29 15:29:03 +0200

    working

--- spark-assignment/main.py
+++ spark-assignment/main.py
@@ -1,7 +1,7 @@
 from pyspark.sql import SparkSession
 import pyspark.sql.types as T
 import pyspark.sql.functions as func
-from pyspark.sql import Row
+
 from pyspark.sql.functions import last,col
 from pyspark.sql.window import Window
 
@@ -88,7 +88,7 @@ def fill(trades, prices):
 
 
 
-def pivot(trades, prices,spark):
+def pivot(trades, prices):
     """
     Pivot and fill the columns on the event id so that each row contains a
     column for each id + column combination where the value is the most recent
@@ -111,44 +111,10 @@ def pivot(trades, prices,spark):
     :param prices: DataFrame of price events
     :return: A DataFrame of the combined events and pivoted columns.
     """
-
-    data = [
-        (10, 1546300800000,None, None, 37.5, 100.000),
-        (10, 1546300801000,None, None, 37.51, 100.000),
-        (20, 1546300804000,None, None, 12.67, 300.000),
-        (10, 1546300807000,None, None, 37.5, 200.000),
-        (10, 1546300799000, 37.5, 37.51,None, None),
-        (10, 1546300802000, 37.51, 37.52,None, None),
-        (10, 1546300806000, 37.5, 37.51,None, None),
-    ]
-    schema = T.StructType(
-        [
-            T.StructField("id", T.LongType()),
-            T.StructField("timestamp", T.LongType()),
-            T.StructField("bid", T.DoubleType()),
-            T.StructField("ask", T.DoubleType()),
-            T.StructField("price", T.DoubleType()),
-            T.StructField("quantity", T.DoubleType()),
-        ]
-    )
-    new_df = spark.createDataFrame(data, schema)
-
-    new_df1 = new_df.groupBy('id','timestamp', 'bid', 'ask', 'price', 'quantity').pivot('id').agg(
-            func.last("bid").alias('bid'),
-            func.last("ask").alias('ask'),
-            func.last('price').alias('price'),
-            func.last('quantity').alias('quantity')) \
-        .orderBy("timestamp")
-    cols = new_df1.columns[6:]
-
-    ffill = Window.orderBy('timestamp').rowsBetween(Window.unboundedPreceding, Window.currentRow)
-    sdf1 = new_df1.select('id', 'timestamp', 'bid', 'ask', 'price', 'quantity',
-                          *[last(col(c), ignorenulls=True).over(ffill).alias(c) for c in cols])
-
-    print("below is second approach")
     id_window = Window.partitionBy('timestamp').orderBy('timestamp').rowsBetween(Window.unboundedPreceding, 0)
 
-    df1 = trades \
+
+    df = trades \
         .withColumn("bid", func.lit(None)) \
         .withColumn("ask", func.lit(None)) \
         .unionByName(
@@ -160,10 +126,7 @@ def pivot(trades, prices,spark):
         .withColumn("ask", func.last("ask", True).over(id_window)) \
         .withColumn("price", func.last("price", True).over(id_window)) \
         .withColumn("quantity", func.last("quantity", True).over(id_window)) \
-        .orderBy("timestamp") \
-        .select("id", "timestamp", "bid", "ask", "price", "quantity")
-
-    df = df1.groupBy('id','timestamp', 'bid', 'ask', 'price', 'quantity').pivot('id').agg(
+        .groupBy('id','timestamp', 'bid', 'ask', 'price', 'quantity').pivot('id').agg(
             func.last("bid",ignorenulls=True).alias('bid'),
             func.last("ask",ignorenulls=True).alias('ask'),
             func.last('price',ignorenulls=True).alias('price'),
@@ -171,7 +134,6 @@ def pivot(trades, prices,spark):
         .orderBy("timestamp")
 
     cols = df.columns[6:]
-
     ffill1 = Window.orderBy('timestamp').rowsBetween(Window.unboundedPreceding, Window.currentRow)
     df = df.select('id', 'timestamp', 'bid', 'ask', 'price', 'quantity',
                           *[last(col(c), ignorenulls=True).over(ffill1).alias(c) for c in cols])
@@ -191,4 +153,4 @@ if __name__ == "__main__":
 
     fill(trades, prices).show()
 
-    pivot(trades, prices,spark).show()
+    pivot(trades, prices).show()
